0ad0eef6319043da888fff0301ebdd79ee1738c8
dgpluks <danielgomespereira2006@gmail.com>
2025-08-20 20:51:18 -0300

    atualizacao leetcode1

--- leetcode1.c
+++ leetcode1.c
@@ -1,15 +1,48 @@
 #include<stdio.h>
+#include <stdlib.h>
+struct no{
+	int valor;
+	int indice;  
+	struct no *proximo;
+};
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+	struct no** lista = calloc(numsSize, sizeof(struct no*));
+	for(int i = 0; i < numsSize; i++){
+		int j = (target-nums[i])%numsSize;
+		if(lista[j]==NULL){   //se o complemento nao exeiste(sem itens)
+			j = nums[i]%numsSize;
+			if(lista[j]==NULL){ //se estiver sem itens na posicao do numero em si
+				struct no *novo = malloc(sizeof(struct no));
+				lista[j] = novo;
+				lista[j]->valor =
+			}
+
+		}
+	}
+	*returnSize = 2;
+
+	return seq;
+}
 int main(){
-	int qn, numeros;
+	int qn;
 	scanf("%d", &qn);
+	int numeros[qn];
 	for(int i = 0; i< qn; i++){
 		scanf("%d", &numeros[i]);
 	}
-}
-int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-	struct no = malloc(sizeof(struct lista));
-}
-struct no{
-	float valor;
-	int *proximo;
+	int alvo;
+	scanf("%d", &alvo);
+	int *seq, tam;
+	seq = twoSum(numeros, qn, alvo, &tam);
+	for(int i = 0; i<tam; i++){
+		if(i==0){
+			printf("[");
+		}
+		printf("%d", seq[i]);
+		if(i!=tam-1){
+			printf(",");
+		}else{
+			printf("]\n");
+		}
+	}
 }
