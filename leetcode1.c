9178cf1e55eaf9c87fae3690a5611cb1553904f1
dgpluks <danielgomespereira2006@gmail.com>
2025-08-20 12:15:13 -0300

    Add files via upload

---
+++ leetcode1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int qn, numeros;
+	scanf("%d", &qn);
+	for(int i = 0; i< qn; i++){
+		scanf("%d", &numeros[i]);
+	}
+}
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+	struct no = malloc(sizeof(struct lista));
+}
+struct no{
+	float valor;
+	int *proximo;
+}
